6fa1c1252ef84aca97f3f3f53276cd29c7afc319
maheswar-dileep <maheaswerd@gmail.com>
2025-01-04 16:47:02 +0530

    fix: 3D model upload fix

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ dotenv.config();
 const PORT = process.env.PORT || 8081;
 
 // Middleware
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({limit: '100mb'}));
 app.use(morgan('tiny'));
 app.use(cors({}));
 
