74fa2db3867759a143df29adb677ffb25ba876f2
maheswar-dileep <maheaswerd@gmail.com>
2025-01-08 14:33:48 +0530

    feat: file upload size updated

--- src/middlewares/multerUpload.ts
+++ src/middlewares/multerUpload.ts
@@ -3,10 +3,7 @@ import { s3Storage } from '../utils/uploadToS3';
 
 export const uploadFile = multer({
   storage: s3Storage,
-//   fileFilter: (req, file, callback) => {
-//     sanitizeFile(file, callback);
-//   },
   limits: {
-    fileSize: 1024 * 1024 * 2 // 2mb file size
+    fileSize: 1024 * 1024 * 10
   }
 });
